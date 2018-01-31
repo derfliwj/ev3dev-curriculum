c0dcc7ff151d672ddeb8f4e19a0d1e8811213321
xiangz2 <xiangz2@rose-hulman.edu>
2018-01-31 13:54:26 -0500

    I add init method in the snatch3r, not finished

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -18,6 +18,13 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
-    
+    def __init__(self):
+        """construct a left motor and a right motor"""
+        self.left_motor = None #nut sure about this line
+
+        
+        assert self.left_motor
+
+
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
