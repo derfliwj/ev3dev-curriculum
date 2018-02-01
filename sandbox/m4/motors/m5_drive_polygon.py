29f527a6f4007c4e3ae542529ecea52f42fdef76
xiangz2 <xiangz2@rose-hulman.edu>
2018-01-31 19:11:47 -0500

    m5_drive polygon update 1

--- sandbox/m4/motors/m5_drive_polygon.py
+++ sandbox/m4/motors/m5_drive_polygon.py
@@ -38,7 +38,7 @@ def main():
         # drive a polygon with the correct number of sides. (Hint: You will add 3 lines of code. What are they?).
         for k in range(sides):
             robot.drive_inches(edge_length_in, speed_deg_per_second)
-            robot.drive_degree(turn_amount)
+            robot.drive_degree(turn_amount, speed_deg_per_second)
             # TODO: 3. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
             #   You are done with the Motors unit!
             #
