dad2484639da63381c8d670f990a8b85dd6b7e2a
derfliwj <derfliwj@rose-hulman.edu>
2018-02-04 15:08:17 -0500

    ALL DONE

--- sandbox/m4/digital_inputs/m6_ir_remote.py
+++ sandbox/m4/digital_inputs/m6_ir_remote.py
@@ -92,7 +92,7 @@ def main():
     # VCS --> Update project...
     # Once the library is implemented any team member should be able to run his code as stated in todo3.
     robot.shutdown()
-    
+
 # ----------------------------------------------------------------------
 # Event handlers
 # Some event handlers have been written for you (ones for the arm).
