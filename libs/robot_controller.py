8860b329498d2193fb06c664944960853cf8a79a
derfliwj <derfliwj@rose-hulman.edu>
2018-02-12 15:09:54 -0500

    ALL DONE

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -161,35 +161,13 @@ class Snatch3r(object):
         turn_speed = 100
 
         while not self.touch_sensor.is_pressed:
-            # The touch sensor can be used to abort the attempt (sometimes handy during testing)
-
-            # DONE: 3. Use the beacon_seeker object to get the current heading and distance.
-            current_heading = beacon_seeker.heading  # use the beacon_seeker heading
-            current_distance = beacon_seeker.distance  # use the beacon_seeker distance
+            current_heading = beacon_seeker.heading
+            current_distance = beacon_seeker.distance
             if current_distance == -128:
-                # If the IR Remote is not found just sit idle for this program until it is moved.
                 print("IR Remote not found. Distance is -128")
                 self.drive_forward(turn_speed, -turn_speed)
             else:
-                # DONE: 4. Implement the following strategy to find the beacon.
-                # If the absolute value of the current_heading is less than 2, you are on the right heading.
-                #     If the current_distance is 0 return from this function, you have found the beacon!  return True
-                #     If the current_distance is greater than 0 drive straight forward (forward_speed, forward_speed)
-                # If the absolute value of the current_heading is NOT less than 2 but IS less than 10, you need to spin
-                #     If the current_heading is less than 0 turn left (-turn_speed, turn_speed)
-                #     If the current_heading is greater than 0 turn right  (turn_speed, -turn_speed)
-                # If the absolute value of current_heading is greater than 10, then stop and print Heading too far off
-                #
-                # Using that plan you should find the beacon if the beacon is in range.  If the beacon is not in range your
-                # robot should just sit still until the beacon is placed into view.  It is recommended that you always print
-                # something each pass through the loop to help you debug what is going on.  Examples:
-                #    print("On the right heading. Distance: ", current_distance)
-                #    print("Adjusting heading: ", current_heading)
-                #    print("Heading is too far off to fix: ", current_heading)
-
-                # Here is some code to help get you started
                 if math.fabs(beacon_seeker.heading) < 2:
-                    # Close enough of a heading to move forward
                     print("On the right heading. Distance: ", current_distance)
                     if beacon_seeker.distance == 1:
                         self.drive_inches(4, turn_speed)
@@ -208,8 +186,6 @@ class Snatch3r(object):
                     self.drive_forward(turn_speed, -turn_speed)
                     print("Heading is too far off to fix: ", current_heading)
             time.sleep(0.2)
-
-        # The touch_sensor was pressed to abort the attempt if this code runs.
         print("Abandon ship!")
         self.stop()
         return False
